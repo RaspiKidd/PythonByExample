491d8167802aaaab17d4c1c1b84f23dce6302653
RaspiKidd <kerry@raspikidd.com>
2019-07-08 11:58:42 +0100

    challenge 26 done and working.

--- Challenge026.py
+++ Challenge026.py
@@ -3,3 +3,17 @@
 # If a word begins with a vowel you just add "way" to the end. For example, pig becomes igpay, banana becomes ananabay,
 # and aadvark becomes aadvarkway. Create a program that will ask the user to enter a word and change it into Pig Latin.
 # Make sure the new word is displayed in lowercase.
+
+word = input("Enter a word: ")
+
+first = word[0]
+length = len(word)
+rest = word [1:length]
+
+if first != "a" and first != "e" and first != "i" and first != "o" and first != "u":
+    newWord = rest + first + "ay"
+else:
+    newWord = word + "way"
+
+
+print(newWord)
